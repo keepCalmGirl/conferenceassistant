bc6a0a9bf35d17db9a2031781488aabb6794f692
keepCalmGirl <ikosjak@gmail.com>
2019-01-24 20:17:21 +0200

    реалізовано методи save, icrementRate, updateStatus, checkEmail в класі QuestionServiceImpl

--- src/main/java/ifit/cluster/cassistant/Service/QuestionService.java
+++ src/main/java/ifit/cluster/cassistant/Service/QuestionService.java
@@ -1,8 +1,13 @@
 package ifit.cluster.cassistant.Service;
 
+import ifit.cluster.cassistant.domain.Question;
+import ifit.cluster.cassistant.domain.Status;
+
 public interface QuestionService {
 
-    void incrementRate();
-    Boolean checkEmail();
+    Question saveQuestion(Question question);
+    void incrementRate(Long question_id);
+    Boolean checkEmail(Long question_id, String email);
+    Status updateStatus(Long question_id, Status status);
 
 }
--- src/main/java/ifit/cluster/cassistant/repository/QuestionRepository.java
+++ src/main/java/ifit/cluster/cassistant/repository/QuestionRepository.java
@@ -1,9 +1,10 @@
 package ifit.cluster.cassistant.repository;
 
+import ifit.cluster.cassistant.domain.Question;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface QuestionRepository extends CrudRepository<Object, Long> {
+public interface QuestionRepository extends CrudRepository<Question, Long> {
 
 }
