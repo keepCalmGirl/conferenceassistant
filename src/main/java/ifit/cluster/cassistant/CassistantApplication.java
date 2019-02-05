cd48cda040eecf497b151cd8a4ca32d84feea821
OlehBiloholovka <35770198+OlehBiloholovka@users.noreply.github.com>
2019-02-05 19:55:45 +0200

    header was fixed

--- src/main/java/ifit/cluster/cassistant/CassistantApplication.java
+++ src/main/java/ifit/cluster/cassistant/CassistantApplication.java
@@ -16,6 +16,7 @@ import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
 import java.util.Date;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class CassistantApplication {
