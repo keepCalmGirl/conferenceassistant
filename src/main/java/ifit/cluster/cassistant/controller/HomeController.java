5286efce8fdf3ff151ba4b3f7c0b303484a9e373
Ivanna Kosiak <ikosjak@gmail.com>
2019-03-17 12:17:33 +0200

    added getHome method to HomeController class changed link in header-Confass on page /home and link in header-Home on page /home

--- src/main/java/ifit/cluster/cassistant/controller/HomeController.java
+++ src/main/java/ifit/cluster/cassistant/controller/HomeController.java
@@ -1,8 +1,13 @@
 package ifit.cluster.cassistant.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @GetMapping("/home")
+    public String getHome(){
+        return "home";
+    }
 }
